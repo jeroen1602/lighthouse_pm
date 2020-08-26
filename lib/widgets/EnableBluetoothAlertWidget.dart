871bc8d996f0444921aa57b564b13c634c83222b
Jeroen van Velzen <jeroen1602@live.nl>
2020-08-26 19:40:40 +0200

    Created Bluetooth not enabled screen.
     - Converted the main activity to the Flutter 2 version.
     - (Android) added option to enable Bluetooth from the Bluetooth not enabled screen.

---
+++ lib/widgets/EnableBluetoothAlertWidget.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+/// An alert dialog to ask the user if they really want to enable the bluetooth
+/// adapter.
+class EnableBluetoothAlertWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+        title: Text('Enable Bluetooth'),
+        content: Text('Do you want to enable Bluetooth?'),
+        actions: <Widget>[
+          FlatButton(
+            child: Text("Enable Bluetooth"),
+            onPressed: () {
+              Navigator.pop(context, true);
+            },
+          ),
+          FlatButton(
+            child: Text("Cancel"),
+            onPressed: () {
+              Navigator.pop(context, false);
+            },
+          ),
+        ]);
+  }
+
+  static Future<bool> showCustomDialog(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return EnableBluetoothAlertWidget();
+        });
+  }
+}
