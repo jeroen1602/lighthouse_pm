cc4b247ad717c0395d55b8048e6091d65d79fd57
Jeroen van Velzen <jeroen1602@live.nl>
2020-09-27 20:58:22 +0200

    Using FormField for id input to validate input on the fly.

--- lib/lighthouseProvider/widgets/ViveBaseStationExtraInfoAlertWidget.dart
+++ lib/lighthouseProvider/widgets/ViveBaseStationExtraInfoAlertWidget.dart
@@ -25,11 +25,43 @@ class ViveBaseStationExtraInfoAlertWidget extends StatefulWidget {
 
 class _ViveBaseStationExtraInfoAlertState
     extends State<ViveBaseStationExtraInfoAlertWidget> {
-  final textController = TextEditingController();
+  final _textController = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
 
   @override
   void initState() {
     super.initState();
+  }
+
+  /// Validate that the id provided is of the correct format
+  ///
+  /// Checks that the length is either 4 or 8.
+  /// Checks that the id is parsable as HEX.
+  /// Checks that the last 4 digits (lower 2 bytes) match the [widget.existingIdEnd]
+  /// (if length is 8).
+  String /* ? */ _validateId(String value) {
+    value = value.trim().toUpperCase();
+    if (value.isEmpty) {
+      return 'Please enter the id';
+    }
+    final intValue = int.tryParse(value, radix: 16);
+    if (intValue == null) {
+      return 'Please use legal characters [a-fA-F0-9]';
+    }
+    if (value.length < 4) {
+      return 'Please enter at least the first 4 characters';
+    }
+    if (value.length > 4 && value.length < 8) {
+      return 'Please enter the rest of the id';
+    }
+    if (value.length == 8 && intValue & 0xFFFF != widget.existingIdEnd) {
+      return 'The last 4 digits don\'t match "${widget.existingIdEnd.toRadixString(16).padLeft(4, '0').toUpperCase()}"';
+    }
+    if (value.length > 8) {
+      return 'The length of the id must be 8 digits';
+    }
+    return null;
   }
 
   @override
@@ -56,10 +88,15 @@ class _ViveBaseStationExtraInfoAlertState
               style: Theme.of(context).primaryTextTheme.bodyText1, text: '.'),
         ]),
       ),
-      content: TextField(
-        decoration: InputDecoration(labelText: 'Id'),
-        controller: textController,
-      ),
+      content: Form(
+          key: _formKey,
+          child: TextFormField(
+            validator: _validateId,
+            onChanged: (String value) {
+              _formKey.currentState.validate();
+            },
+            controller: _textController,
+          )),
       actions: <Widget>[
         SimpleDialogOption(
           child: Text('Cancel'),
@@ -68,10 +105,8 @@ class _ViveBaseStationExtraInfoAlertState
         SimpleDialogOption(
           child: Text('Set'),
           onPressed: () {
-            final text = textController.text.trim();
-            if (text.isEmpty) {
-              Navigator.pop(context);
-            } else {
+            final text = _textController.text.trim();
+            if (_formKey.currentState.validate()) {
               Navigator.pop(context, text);
             }
           },
@@ -82,7 +117,7 @@ class _ViveBaseStationExtraInfoAlertState
 
   @override
   void dispose() {
-    textController.dispose();
+    _textController.dispose();
     super.dispose();
   }
 }
