28fd513134de59b2934266a52444d75030b56ea0
Jeroen van Velzen <jeroen1602@live.nl>
2021-04-06 15:57:31 +0200

    Updated web db name

--- lib/data/shared/web.dart
+++ lib/data/shared/web.dart
@@ -3,5 +3,5 @@ import 'package:moor/moor_web.dart';
 import '../Database.dart';
 
 LighthouseDatabase constructDb({bool logStatements = false}) {
-  return LighthouseDatabase(WebDatabase('db', logStatements: logStatements));
+  return LighthouseDatabase(WebDatabase('LighthouseDatabase', logStatements: logStatements));
 }
