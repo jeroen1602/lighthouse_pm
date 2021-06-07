a4c4da42defcf65464652e5fc87b8b0d39070527
Jeroen van Velzen <jeroen1602@live.nl>
2021-06-07 11:21:44 +0200

    Added tests for device extensions
    Added tests for ClearIdExtension
    Added tests for DeviceExtenion
    Added tests for IdentifyDeviceExtension
    Added tests for ShortcutExtension
    Added tests for StateExtension
    Added tests for OnExtension
    Added tests for SleepExtension
    Added tests for StandbyExtension

--- test/helpers/FakeBloc.dart
+++ test/helpers/FakeBloc.dart
@@ -1,7 +1,9 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:lighthouse_pm/bloc.dart';
+import 'package:lighthouse_pm/data/Database.dart';
 import 'package:lighthouse_pm/data/dao/SettingsDao.dart';
 import 'package:lighthouse_pm/data/dao/ViveBaseStationDao.dart';
+import 'package:rxdart/rxdart.dart';
 
 class FakeBloc extends Fake implements LighthousePMBloc {
   FakeBloc(this.internalSettingsDao, this.internalViveBaseStation);
@@ -14,10 +16,10 @@ class FakeBloc extends Fake implements LighthousePMBloc {
   final FakeViveBaseStationDao internalViveBaseStation;
 
   @override
-  SettingsDao get settings => internalSettingsDao;
+  FakeSettingsDao get settings => internalSettingsDao;
 
   @override
-  ViveBaseStationDao get viveBaseStation => internalViveBaseStation;
+  FakeViveBaseStationDao get viveBaseStation => internalViveBaseStation;
 }
 
 class FakeSettingsDao extends Fake implements SettingsDao {
@@ -33,5 +35,50 @@ class FakeViveBaseStationDao extends Fake implements ViveBaseStationDao {
   @override
   Future<int?> getId(String deviceId) async {
     return null;
+  }
+
+  BehaviorSubject<List<ViveBaseStationId>>? idsStream;
+
+  @override
+  Future<void> insertId(String deviceId, int id) async {
+    final idsStream = this.idsStream;
+    if (idsStream != null) {
+      final index = idsStream.value
+              ?.indexWhere((element) => element.deviceId == deviceId) ??
+          -1;
+      if (index >= 0) {
+        idsStream.value?.removeAt(index);
+      }
+      idsStream.value
+          ?.add(ViveBaseStationId(deviceId: deviceId, baseStationId: id));
+      idsStream.add(idsStream.value ?? []);
+    }
+  }
+
+  @override
+  Future<void> deleteId(String deviceId) async {
+    final idsStream = this.idsStream;
+    if (idsStream != null) {
+      final index = idsStream.value
+              ?.indexWhere((element) => element.deviceId == deviceId) ??
+          -1;
+      if (index >= 0) {
+        idsStream.value?.removeAt(index);
+        idsStream.add(idsStream.value ?? []);
+      }
+    }
+  }
+
+  @override
+  Stream<List<ViveBaseStationId>> getViveBaseStationIdsAsStream() {
+    startViveBaseStationIdsStream();
+    return idsStream!.stream;
+  }
+
+  void startViveBaseStationIdsStream(
+      [List<ViveBaseStationId> data = const []]) {
+    if (idsStream == null) {
+      idsStream = BehaviorSubject.seeded(data.toList());
+    }
   }
 }
