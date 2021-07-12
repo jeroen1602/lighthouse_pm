d8d32178efe4ce62bc1736cc5791dcbc85faa62f
Jeroen <Jeroen1602@live.nl>
2021-07-12 14:09:50 +0200

    Added support for bluez back end

---
+++ lib/lighthouseProvider/backEnd/BlueZ/BlueZBluetoothDevice.dart
@@ -0,0 +1,67 @@
+import 'dart:async';
+
+import 'package:bluez/bluez.dart';
+import 'package:rxdart/subjects.dart';
+
+import '../../ble/BluetoothDevice.dart';
+import '../../ble/BluetoothService.dart';
+import '../../ble/DeviceIdentifier.dart';
+import 'BlueZBluetoothService.dart';
+
+class BlueZBluetoothDevice extends LHBluetoothDevice {
+  BlueZBluetoothDevice(this.device) {
+    _stateSubject.onCancel = () {
+      _stateStream?.cancel();
+      _stateStream = null;
+    };
+  }
+
+  final BlueZDevice device;
+
+  @override
+  Future<void> connect({Duration? timeout}) async {
+    if (timeout != null) {
+      await device.connect().timeout(timeout);
+    } else {
+      await device.connect();
+    }
+  }
+
+  @override
+  Future<void> disconnect() async {
+    await device.disconnect();
+  }
+
+  @override
+  Future<List<LHBluetoothService>> discoverServices() async {
+    return device.gattServices.map((e) => BlueZBluetoothService(e)).toList();
+  }
+
+  @override
+  LHDeviceIdentifier get id => LHDeviceIdentifier(device.address);
+
+  @override
+  String get name => device.name;
+
+  BehaviorSubject<LHBluetoothDeviceState> _stateSubject = BehaviorSubject();
+  StreamSubscription<LHBluetoothDeviceState>? _stateStream;
+
+  void _startStateStream() {
+    if (_stateStream == null) {
+      _stateStream = Stream.periodic(Duration(seconds: 2))
+          .map((event) => device.connected)
+          .map((event) => event
+              ? LHBluetoothDeviceState.connected
+              : LHBluetoothDeviceState.disconnected)
+          .listen((event) {
+        _stateSubject.add(event);
+      });
+    }
+  }
+
+  @override
+  Stream<LHBluetoothDeviceState> get state {
+    _startStateStream();
+    return _stateSubject.stream;
+  }
+}
--- lib/lighthouseProvider/backEnd/BlueZBackEndIO.dart
+++ lib/lighthouseProvider/backEnd/BlueZBackEndIO.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:bluez/bluez.dart';
+import 'package:lighthouse_pm/lighthouseProvider/backEnd/BlueZ/BlueZBluetoothDevice.dart';
+import 'package:mutex/mutex.dart';
 import 'package:rxdart/rxdart.dart';
 
 import '../LighthouseDevice.dart';
@@ -19,15 +23,25 @@ class BlueZBackEnd extends BLELighthouseBackEnd {
     return _instance!;
   }
 
+  // Some state variables.
+  final Mutex _devicesMutex = Mutex();
+
+  BehaviorSubject<LighthouseDevice?> _foundDeviceSubject =
+      BehaviorSubject.seeded(null);
+  StreamSubscription? _scanResultSubscription;
   final BlueZClient blueZClient = BlueZClient();
 
-  @override
-  // TODO: implement lighthouseStream
-  Stream<LighthouseDevice?> get lighthouseStream => throw UnimplementedError();
+  StreamSubscription<void>? _stopScan;
+
+  BehaviorSubject<bool> _scanningSubject = BehaviorSubject.seeded(false);
 
   @override
-  Stream<BluetoothAdapterState> get state {
-    return MergeStream<dynamic>([
+  Stream<LighthouseDevice?> get lighthouseStream => _foundDeviceSubject.stream;
+
+  @override
+  Stream<BluetoothAdapterState> get state async* {
+    await _ensureConnected();
+    yield* MergeStream<dynamic>([
       Stream.value(blueZClient.devices), // Always fires at least once
       blueZClient.adapterAdded,
       blueZClient.adapterRemoved
@@ -46,9 +60,106 @@ class BlueZBackEnd extends BLELighthouseBackEnd {
     });
   }
 
+  Stream<bool>? get isScanning => _scanningSubject.stream;
+
   @override
-  Future<void> stopScan() {
-    // TODO: implement stopScan
-    throw UnimplementedError();
+  Future<void> startScan(
+      {required Duration timeout, required Duration? updateInterval}) async {
+    await super.startScan(timeout: timeout, updateInterval: updateInterval);
+    await _ensureConnected();
+    await _startListeningScanResults();
+    try {
+      final adapters =
+          blueZClient.adapters.where((adapter) => adapter.powered).toList();
+      if (adapters.isNotEmpty) {
+        _scanningSubject.add(true);
+      }
+      await Future.wait(adapters.map((adapter) {
+        // TODO: we may need this, but probably not.
+        //adapter.setDiscoveryFilter(filter);
+        return adapter.startDiscovery();
+      }));
+      _stopScan = Future.delayed(timeout).asStream().listen((event) {
+        _stopScan = null;
+        _scanningSubject.add(false);
+        stopScan();
+      });
+    } catch (e, s) {
+      print("Error with BlueZ scanning $e\n$s");
+    }
+  }
+
+  @override
+  Future<void> cleanUp() async {}
+
+  @override
+  Future<void> stopScan() async {
+    await _ensureConnected();
+    _scanResultSubscription?.pause();
+    _stopScan?.cancel();
+    _stopScan = null;
+    await Future.wait(blueZClient.adapters
+        .where((adapter) => adapter.powered && adapter.discovering)
+        .map((adapter) async {
+      try {
+        await adapter.stopDiscovery();
+      } catch (e, s) {
+        print(
+            "Cannot stop discovery because: ${e.toString()}\n${s.toString()}");
+      }
+    }));
+    _scanningSubject.add(false);
+  }
+
+  Future<void> _startListeningScanResults() async {
+    var scanResultSubscription = _scanResultSubscription;
+    if (scanResultSubscription != null) {
+      if (!scanResultSubscription.isPaused) {
+        scanResultSubscription.pause();
+      }
+      await scanResultSubscription.cancel();
+      _scanResultSubscription = null;
+    }
+
+    scanResultSubscription = MergeStream(
+            [Stream.fromIterable(blueZClient.devices), blueZClient.deviceAdded])
+        .where((device) {
+          //TODO: Maybe move this to be a bit more generic.
+          // Filter out all devices that don't have a correct name.
+          for (final deviceProvider in providers) {
+            if (deviceProvider.nameCheck(device.name)) {
+              return true;
+            }
+          }
+          return false;
+        })
+        // Give the listener at least 2ms to process the data before firing again.
+        .debounce((_) => TimerStream(true, Duration(milliseconds: 2)))
+        .listen((device) async {
+          await device.setTrusted(true);
+          final lighthouseDevice =
+              await getLighthouseDevice(BlueZBluetoothDevice(device));
+          try {
+            await _devicesMutex.acquire();
+            if (lighthouseDevice == null) {
+              print('Found a non valid device! Device id: ${device.address}');
+            } else {
+              this._foundDeviceSubject.add(lighthouseDevice);
+            }
+          } finally {
+            if (_devicesMutex.isLocked) {
+              _devicesMutex.release();
+            }
+          }
+        });
+    // Clean-up for when the stream is canceled.
+    scanResultSubscription.onDone(() {
+      this._scanResultSubscription = null;
+    });
+    this._scanResultSubscription = scanResultSubscription;
+  }
+
+  Future<void> _ensureConnected() async {
+    await blueZClient.connect();
   }
 }
