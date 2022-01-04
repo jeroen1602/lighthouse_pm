9b664d5a083761230d70fab43f5b21b0ec5b5b09
Jeroen1602 <1492076+jeroen1602@users.noreply.github.com>
2022-01-04 20:09:52 +0100

    Added test for privacy page

--- test/pages/privacy_page_test.dart
+++ test/pages/privacy_page_test.dart
@@ -1,5 +1,10 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:lighthouse_pm/pages/settings/privacy_page.dart';
+import 'package:lighthouse_pm/widgets/content_container_widget.dart';
+
+import '../helpers/widget_helpers.dart';
 
 void main() {
   test('Should generate correct super script', () {
@@ -11,5 +16,82 @@ void main() {
 
     final script3 = PrivacyPage.toSuperScript(-40);
     expect(script3, equals("⁻⁴⁰"));
+  });
+
+  testWidgets('Should create privacy page', (WidgetTester tester) async {
+    await tester.pumpWidget(buildTestApp((context) {
+      return PrivacyPage();
+    }));
+
+    await tester.pumpAndSettle();
+
+    expect(find.text("Privacy"), findsOneWidget, reason: "Should find heading");
+    expect(find.textContaining(PrivacyPage.version1_1Date), findsOneWidget,
+        reason: "Should find privacy versions");
+  });
+
+  testWidgets('Should open links', (WidgetTester tester) async {
+    await tester.pumpWidget(buildTestApp((context) {
+      return PrivacyPage();
+    }));
+
+    await tester.pumpAndSettle();
+
+    final list = tester.firstWidget(find.byType(ListView));
+
+    final containerWidget = tester
+        .widgetList(find.descendant(
+            of: find.byWidget(list),
+            matching: find.byType(ContentContainerWidget)))
+        .toList()[1];
+    expect(containerWidget, isNotNull);
+
+    final RichText richText = tester
+        .widgetList<RichText>(find.descendant(
+            of: find.byWidget(containerWidget),
+            matching: find.byType(RichText)))
+        .toList()[0];
+
+    final TextSpan text = richText.text as TextSpan;
+
+    // print(text.toPlainText());
+
+    TextSpan? githubPrivacyLink;
+    TextSpan? switchNewPhoneLink;
+    TextSpan? googlePrivacyLink;
+
+    for (final element in text.children!) {
+      if (element is TextSpan) {
+        if (element.text == "Github privacy statement") {
+          githubPrivacyLink = element;
+        } else if (element.text == "Switch to a new Android phone") {
+          switchNewPhoneLink = element;
+        } else if (element.text == "Google privacy Statement") {
+          googlePrivacyLink = element;
+        }
+      }
+    }
+
+    expect(githubPrivacyLink, isNotNull,
+        reason: "Github privacy link should exist");
+    expect(switchNewPhoneLink, isNotNull,
+        reason: "Switch new phone link should exist");
+    expect(googlePrivacyLink, isNotNull,
+        reason: "Google privacy link should exist");
+
+    expect(githubPrivacyLink!.recognizer, isA<TapGestureRecognizer>());
+    expect(switchNewPhoneLink!.recognizer, isA<TapGestureRecognizer>());
+    expect(googlePrivacyLink!.recognizer, isA<TapGestureRecognizer>());
+
+    expect((githubPrivacyLink.recognizer as TapGestureRecognizer).onTap,
+        isNotNull);
+    expect((switchNewPhoneLink.recognizer as TapGestureRecognizer).onTap,
+        isNotNull);
+    expect((googlePrivacyLink.recognizer as TapGestureRecognizer).onTap,
+        isNotNull);
+
+    (githubPrivacyLink.recognizer as TapGestureRecognizer).onTap!();
+    (switchNewPhoneLink.recognizer as TapGestureRecognizer).onTap!();
+    (googlePrivacyLink.recognizer as TapGestureRecognizer).onTap!();
   });
 }
